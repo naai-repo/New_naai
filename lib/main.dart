510db609740ee83ff76fee8f01445834ea70f03b
Jatin <jatinkumarmid@gmail.com>
2024-02-29 19:35:49 +0530

    changes for publish ..

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,7 @@ class MyApp extends StatelessWidget {
           ChangeNotifierProvider(create: (_) => BookingServicesSalonProvider()),
           ChangeNotifierProvider(create: (_) => BookingScreenChangeProvider()),
       ],
+      
       child: ScreenUtilInit(
         designSize: const Size(414,896),
         splitScreenMode: true,
